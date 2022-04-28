05976ad9a62020b22129e419ec1b737e2a15760a
robyn-box <robyn.mateo@gmail.com>
2022-04-28 00:03:13 -1000

    updates

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose')
 const hbs = require('handlebars')
+const saltRounds = +process.env.SALT
+const secret = process.env.SECRET
 
 const indexRouter = require('./routes/index');
 const aboutRouter = require('./routes/about');
 var usersRouter = require('./routes/users');
-// const createHeroRouter = require('./routes/createHero')
+const createHeroRouter = require('./routes/createHero')
 // const createVillianRouter = require('./routes/createVillian')
 // const heroPowerCreateRouter = require('./routes/heroPowerCreate');
 // const villianPowerCreateRouter = require('./routes/villainPowerCreate')
@@ -19,8 +21,20 @@ var usersRouter = require('./routes/users');
 
 const app = express();
 
+// Mongo DB connection
+mongoose.connect(process.env.DB_URI , {
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(res => console.log("DB Connected"))
+.catch(err => console.log(err))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// hbs.registerPartials(__dirname + /views/partials)
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -32,7 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
-// app.use('/createHero', createHeroRouter);
+app.use('/createHero', createHeroRouter);
 // app.use('/createVillian', createVillianRouter);
 // app.use('/heroPowerCreate', heroPowerCreateRouter);
 // app.use('/villianPowerCreate', villianPowerCreateRouter);
@@ -42,9 +56,8 @@ app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  // next(createError(404))
-  // res.status(404);
-  res.render('404')
+  next(createError(404))
+  
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const Superhero = require('../models/Superhero');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SUPERHERO VS SUPERVILLIAN' });
+router.get('/', async function(req, res, next) {
+  const superheroes = await Superhero.find({})
+  res.render('index', {superheroes})
 });
 
 module.exports = router;
